5869ad11731f3dc4b41e8abd217b9573b9e17fe9
colour10 <84299915@qq.com>
2020-08-27 06:45:41 +0000

    基础页面

---
+++ app/Http/Controllers/StaticPagesController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+/**
+ * 单页
+ *
+ * Class StaticPagesController
+ * @package App\Http\Controllers
+ */
+class StaticPagesController extends Controller
+{
+    public function home()
+    {
+        return view('static_pages/home');
+    }
+
+    public function help()
+    {
+        return view('static_pages/help');
+    }
+
+    public function about()
+    {
+        return view('static_pages/about');
+    }
+}
---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>@yield('title', 'Weibo app') - laravel新手教程</title>
+</head>
+<body>
+@yield('content')
+</body>
+</html>
